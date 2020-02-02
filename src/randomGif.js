8c690df37d893dc04e92da0f828c81bdaedbbafb
SiranWeb <kirillg.web@gmail.com>
2020-02-02 19:42:07 +0300

    new UI and href for gif

--- src/randomGif.js
+++ src/randomGif.js
@@ -1,12 +1,14 @@
 const app           = document.querySelector('#app');
 const getGifBtn     = document.querySelector('#getGif');
 const imgRandomGif  = document.querySelector('#randomGif');
-
+const imgRandomGifLink = document.querySelector('#randomGif-container');
 
 function getAndShowNewRandomGif() {
     removeRandomGif();
     getRandomGif();
 };
+
+
 
 function getRandomGif() {
     const url = 'https://api.giphy.com/v1/gifs/random?api_key=cRZXFakx9LE0r5Obsnt8ZeL7cSa9tofQ&tag=&rating=G';
@@ -17,10 +19,13 @@ function getRandomGif() {
 
 function removeRandomGif() {
     imgRandomGif.src = '';
+    imgRandomGifLink.href = '';
 };
 
 function showRandomGif(dataJson) {
-    imgRandomGif.src = dataJson.data.image_original_url;
+    let url = dataJson.data.image_original_url;
+    imgRandomGif.src = url;
+    imgRandomGifLink.href = url;
 };
 
 getGifBtn.addEventListener('click', getAndShowNewRandomGif);
